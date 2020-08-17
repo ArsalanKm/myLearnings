7320910cd81ab94305675542144fe6623049a834
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-08-17 10:58:46 +0430

    random shapes with click and drop options created with matter.js library

---
+++ javaScript/maze/index.js
@@ -0,0 +1,61 @@
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies,
+  MouseConstraint,
+  Mouse,
+} = Matter;
+const width = 800;
+const height = 600;
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: width,
+    height: height,
+    wireframes: false,
+  },
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
+// walls
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+];
+
+World.add(world, walls);
+
+// Random Shapes
+
+for (let i = 0; i < 20; i++) {
+  if (Math.random() > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
+    );
+  } else {
+    World.add(
+      world,
+      Bodies.circle(Math.random() * width, Math.random() * height, 35,{
+          render:{
+              fillStyle:'red'
+          }
+      })
+    );
+  }
+}
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));

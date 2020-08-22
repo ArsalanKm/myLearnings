9bb4e3cdc29423d8dc24cca235a33507d053eb10
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-08-22 08:45:57 +0430

    colorfull listing is available now

--- javaScript/list/index.js
+++ javaScript/list/index.js
@@ -1,28 +1,38 @@
 #!/usr/bin/env node
 const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const path = require("path");
+const lstat = util.promisify(fs.lstat);
+// console.log(fs);
+// method3
+// const{lstat} = fs.promises;
+// method 1
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) reject(err);
+//     });
+//     resolve(stats);
+//   });
+// };
+const targetDir = process.argv[2] || process.cwd();
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
+  const statPromises = filenames.map((filename) => {
+    return lstat(path.join(targetDir, filename));
+  });
+  const allStats = await Promise.all(statPromises);
 
-  const allStats = Array(filenames.length).fill(null);
-
-  for (let filename of filenames) {
-    const index = filenames.indexOf(filename);
-    fs.lstat(filename, (err, stats) => {
-      if (err) console.log(err);
-      allStats[index] = stats;
-      const ready = allStats.every((stats) => {
-        return stats;
-      });
-      if (ready) {
-        allStats.forEach((stats, index) => {
-          console.log(filenames[index], stats.isFile());
-        });
-      }
-    });
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+    if (stats.isFile()) {
+      console.log(filenames[index]);
+    } else {
+      console.log(chalk.red(filenames[index]));
+    }
   }
-
-  //   solution 1
 });

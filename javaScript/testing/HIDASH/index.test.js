3088164e50557508c39a440deb71f8d4f94722f0
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-08-29 10:53:04 +0430

    refatored to mocha library just by replacing two words

--- javaScript/testing/HIDASH/index.test.js
+++ javaScript/testing/HIDASH/index.test.js
@@ -1,30 +1,30 @@
 const assert = require("assert");
 const {forEach, map} = require("./index");
-const test = (desc, fn) => {
-    console.log("---", desc);
-    try {
-        fn();
-    } catch (err) {
-        console.log(err.message);
-    }
-};
+// const test = (desc, fn) => {
+//     console.log("---", desc);
+//     try {
+//         fn();
+//     } catch (err) {
+//         console.log(err.message);
+//     }
+// };
 
-test("The foreach function", () => {
+it("The foreach function", () => {
     let sum = 0;
     forEach([1, 2, 3], (value) => {
         sum += value;
     });
 
-    assert.strictEqual(sum, 8, "sum must be 6 ")
+    assert.strictEqual(sum, 6, "sum must be 6 ")
 
     // if (sum !== 7) throw new Error("Expected summing array be 6");
 });
 
-test("The map function", () => {
+it("The map function", () => {
     const result = map([1, 2, 3], (value) => {
         return value * value;
     });
-    assert.deepStrictEqual(result, [2, 4, 6])
+    assert.deepStrictEqual(result, [1, 4, 9])
     // assert.strictEqual(result[0], 1)
     // assert.strictEqual(result[1], 4)
     // assert.strictEqual(result[2], 9)

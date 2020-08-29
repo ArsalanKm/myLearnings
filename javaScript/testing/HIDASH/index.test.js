97a14660ea2f7a3209d3cf88aaa3e3e0fc989b11
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-08-29 10:33:15 +0430

    refactor testing with assert module

--- javaScript/testing/HIDASH/index.test.js
+++ javaScript/testing/HIDASH/index.test.js
@@ -1,30 +1,32 @@
-const { forEach, map } = require("./index");
+const assert = require("assert");
+const {forEach, map} = require("./index");
 const test = (desc, fn) => {
-  console.log("---", desc);
-  try {
-    fn();
-  } catch (err) {
-    console.log(err.message);
-  }
+    console.log("---", desc);
+    try {
+        fn();
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 test("The foreach function", () => {
-  let sum = 0;
-  forEach([1, 2, 3], (value) => {
-    sum += value;
-  });
-  if (sum !== 7) throw new Error("Expected summing array be 6");
+    let sum = 0;
+    forEach([1, 2, 3], (value) => {
+        sum += value;
+    });
+
+    assert.strictEqual(sum, 8, "sum must be 6 ")
+
+    // if (sum !== 7) throw new Error("Expected summing array be 6");
 });
 
 test("The map function", () => {
-  const result = map([1, 2, 3], (value) => {
-    return value * value;
-  });
+    const result = map([1, 2, 3], (value) => {
+        return value * value;
+    });
+    assert.deepStrictEqual(result, [2, 4, 6])
+    // assert.strictEqual(result[0], 1)
+    // assert.strictEqual(result[1], 4)
+    // assert.strictEqual(result[2], 9)
 
-  if (result[0] !== 1)
-    throw new Error(`result 0 must be 1 but found ${result[0]}`);
-  if (result[1] !== 4)
-    throw new Error(`result 1 must be 4 but found ${result[1]}`);
-  if (result[2] !== 9)
-    throw new Error(`result 2 must be 9 but found ${result[2]}`);
 });

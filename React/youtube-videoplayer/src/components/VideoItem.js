6817a5377bea145bb9b3d87f4baecd00d075f01c
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-09-05 09:34:02 +0430

    video player added to the website

--- React/youtube-videoplayer/src/components/VideoItem.js
+++ React/youtube-videoplayer/src/components/VideoItem.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const VideoItem = ({video, onVideoSelect}) => {
     return (
-        <div onClick={() => onVideoSelect(video)} className='video-item item'>
+        <div onClick={() => {onVideoSelect(video)}} className='video-item item'>
             <img className='ui image'
                  src={video.snippet.thumbnails.medium.url}
                  alt={video.snippet.title}/>

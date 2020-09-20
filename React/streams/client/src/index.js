a4215490e5dcc49d2eec51a50e470ce99bece944
Arsalan Karimzad <karimzadarsalan@gmail.com>
2020-09-20 18:21:26 +0430

    redux dev tools added to project

--- React/streams/client/src/index.js
+++ React/streams/client/src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import reducers from './reducers'
 
-const store = createStore(reducers)
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+    reducers,
+    composeEnhances(applyMiddleware())
+)
 ReactDOM.render(
     <Provider store={store}>
         <App/>
